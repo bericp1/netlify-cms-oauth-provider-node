f557dedf28964c1be28af000f360ebd46f55131c
Brandon Phillips <bericp1@gmail.com>
2019-11-30 13:58:29 -0700

    feat(*): base setup
    Includes:
    
    -   eslint
    -   prettier
    -   husky + lint-staged
    -   jest
    -   semantic-release

---
+++ src/createHandlers.js
@@ -0,0 +1,11 @@
+/**
+ * Create the OAuth flow handlers.
+ *
+ * @param {object} userConfig
+ * @return {boolean}
+ */
+function createHandlers(userConfig = {}) {
+    return true;
+}
+
+module.exports = createHandlers;
